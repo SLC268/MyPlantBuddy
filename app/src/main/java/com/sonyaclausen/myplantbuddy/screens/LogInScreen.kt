c78aeb4f91a9589919a26537a0daa2f9cd0bc05c
Sonya <eaasocl@students.eaaa.dk>
2024-06-23 09:21:03 +0200

    new loginbutton

--- app/src/main/java/com/sonyaclausen/myplantbuddy/screens/LogInScreen.kt
+++ app/src/main/java/com/sonyaclausen/myplantbuddy/screens/LogInScreen.kt
@@ -8,8 +8,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -18,11 +24,17 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -94,26 +106,60 @@ private fun LoginFields() {
                 .padding(4.dp)
                 .width(250.dp)
         )
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text(text= stringResource(id = R.string.password)) },
-            modifier = Modifier
-                .padding(4.dp)
-                .width(250.dp)
-        )
+        ShowHidePassword()
     }
+}
+
+@Composable
+private fun ShowHidePassword() {
+    var password by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(value = false) }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .padding(4.dp)
+            .width(250.dp),
+        value = password,
+        onValueChange = { newText ->
+            password = newText
+        },
+        label = {
+            Text(text = stringResource(id = R.string.password))
+        },
+        placeholder = { Text(text = "Type password here") },
+        visualTransformation = if (showPassword) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            if (showPassword) {
+                IconButton(onClick = { showPassword = false }) {
+                    Icon(
+                        imageVector = Icons.Filled.Visibility,
+                        contentDescription = "hide_password"
+                    )
+                }
+            } else {
+                IconButton(
+                    onClick = { showPassword = true }) {
+                    Icon(
+                        imageVector = Icons.Filled.VisibilityOff,
+                        contentDescription = "hide_password"
+                    )
+                }
+            }
+        }
+    )
+
 }
 
 @Composable
 private fun LoginButtons(onClick: () -> Unit) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(
-            onClick = { /* TODO */ },
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-        ) {
-            Text(text = stringResource(id =R.string.login))
+        LoginButton {
+            {}
         }
         Button(
             onClick = onClick,
@@ -138,6 +184,17 @@ private fun LoginButtons(onClick: () -> Unit) {
             modifier = Modifier
                 .padding(16.dp)
                 .clickable { /* TODO */ })
+    }
+}
+
+@Composable
+private fun LoginButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+    ) {
+        Text(text = stringResource(id = R.string.login))
     }
 }
 
